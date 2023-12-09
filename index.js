3b1ebffdef6f7ce86d20262bb17babae5b950d20
Jasper Wong <wlcjasper@gmail.com>
2023-12-09 12:29:47 +0800

    second commit

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const app = express();
 
 // add route handler
 // req is info from users.  Res is info from server from use.
-app.get('/', (req, res) =>{
-  res.send('hi there');
+app.get('/', (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST">
+      <input name ="email" placeholder="email" />
+      <input name ="password" placeholder="password" />
+      <input name= "passwordConfirmation"placeholder="password confirm" />
+      <button>Sign up</button>
+    </form>
+  </div>
+  `);
 })
 
-app.listen(3000, ()=> {
+app.post('/', (req, res) =>{
+  res.on('data', (data) => {
+    console.log(data);
+  })
+  res.send("Account created!")
+})
+
+app.listen(3000, () => {
   console.log('Listening');
 })
