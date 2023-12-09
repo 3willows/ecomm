fe734183069ccbd22163fc4eae4d1581e4954907
Jasper Wong <wlcjasper@gmail.com>
2023-12-09 14:14:35 +0800

    use library middleware function instead

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 // app is the object we will be modified
 
@@ -19,25 +20,8 @@ app.get('/', (req, res) => {
   `);
 })
 
-const bodyParser = (req, res, next) => {
-  if (req.method = "post") {
-    req.on('data', data => {
-      const parsed = data.toString('utf8').split('&');
-      const formData = {};
-      for (let pair of parsed) {
-        const [key, value] = pair.split('=');
-        formData[key] = value
-      }
-      req.body = formData;
-      next();
-    })
-  }
-  else {
-    next();
-  }
-}
 
-app.post('/', bodyParser, (req, res) => {
+app.post('/', bodyParser.urlencoded({extended: true}), (req, res) => {
   console.log(req.body);
   res.send("account created")
 })
