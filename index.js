c8d604ef970b8740664c0a273fb2f50c9845018a
Jasper Wong <wlcjasper@gmail.com>
2023-12-09 14:10:23 +0800

    add middleware function

--- index.js
+++ index.js
@@ -19,21 +19,29 @@ app.get('/', (req, res) => {
   `);
 })
 
-app.post('/', (req, res) =>{
-  req.on('data', data => {
-    console.log(typeof data);
-    console.log(data);
-     
-    const parsed = data.toString('utf8').split('&');
-    const formData = {};
-    for (let pair of parsed){
-      const [key, value] = pair.split('=');
-      formData[key] = value
-    }
-    console.log(formData);
-  })
-  res.send("Account created!")
+const bodyParser = (req, res, next) => {
+  if (req.method = "post") {
+    req.on('data', data => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value
+      }
+      req.body = formData;
+      next();
+    })
+  }
+  else {
+    next();
+  }
+}
+
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
+  res.send("account created")
 })
+
 
 app.listen(3000, () => {
   console.log('nodemon is listening!');
