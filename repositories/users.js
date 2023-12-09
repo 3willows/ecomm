bdc3ddeeb15e38d26964efed4411b8057ce31730
Jasper Wong <wlcjasper@gmail.com>
2023-12-09 15:43:23 +0800

    add getAll function

--- repositories/users.js
+++ repositories/users.js
@@ -1,3 +1,4 @@
+const { isUtf8 } = require('buffer');
 const fs = require('fs');
 
 class UsersRepository {
@@ -13,6 +14,22 @@ class UsersRepository {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  async getAll() {
+    // open the file called this.filename
+    // read its contents
+    const content = await fs.promises.readFile(this.filename, { encoding: 'utf8' });
+    // parse the contents
+    const data = JSON.parse(content);
+    // return the parsed data
+    return data;
+  }
 }
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+  const users = await repo.getAll();
+  console.log(users);
+}
+
+test()
