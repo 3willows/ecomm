a1311faa50be15fd632bf9623efb77fd5930a4f9
Jasper Wong <wlcjasper@gmail.com>
2023-12-09 16:16:34 +0800

    add create function

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,6 @@
 const { isUtf8 } = require('buffer');
 const fs = require('fs');
+const { stringify } = require('querystring');
 
 class UsersRepository {
   constructor(filename) {
@@ -24,12 +25,26 @@ class UsersRepository {
     // return the parsed data
     return data;
   }
+
+  async create(attributes){
+    const records = await this.getAll();
+    records.push(attributes);
+    await fs.promises.writeFile(this.filename, JSON.stringify(records), {econding: 'utf8'});
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
+
   const users = await repo.getAll();
+ 
   console.log(users);
+
+  await repo.create({email: ... @ ... password: ...});
+
+  const updatedUsers = await repo.getAll(); // Fetch updated users
+
+  console.log(updatedUsers); // Log the updated users
 }
 
 test()
