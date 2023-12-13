0c23466ee7ac80de3ad05851074aeaf86c4276e8
Jasper Wong <wlcjasper@gmail.com>
2023-12-14 01:41:13 +0800

    demonstrate error due to body parser not being able to process multipart data

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,26 +6,29 @@ const router = express.Router()
 const productsRepo = require('../../repositories/products')
 const productsNewTemplate = require('../../views/products/new')
 
-const {
-  requireTitle,
-  requirePrice
-} = require('./validators')
+const { requireTitle, requirePrice } = require('./validators')
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}))
 })
 
-router.post('/admin/products/new', [requireTitle, requirePrice], async (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.send(productsNewTemplate({ errors }))
+router.post(
+  '/admin/products/new',
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req)
+    console.log(req.body)
+    console.log(errors)
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }))
+    }
+    if (errors.isEmpty()) {
+      const { title, price } = req.body
+      await productsRepo.create({ title, price })
+      return res.redirect('/admin/products/new')
+    }
   }
-  if (errors.isEmpty()) {
-  const { title, price } = req.body
-  await productsRepo.create({ title, price })
-  return res.redirect('/admin/products/new')
-}})
-
+)
 
 router.get('/admin/products', (req, res) => {})
 
--- views/products/new.js
+++ views/products/new.js
@@ -4,12 +4,12 @@ const { getError } = require('../../helpers')
 module.exports = ({ errors }) => {
   return layout({
     content: `
-    <form method ="POST">
+    <form method ="POST" enctype = "multipart/form-data" >
     <input placeholder ="Title" name = "title" />
-    <p class="help is-danger">${getError(errors, 'title')}</p>
+    // <p class="help is-danger">${getError(errors, 'title')}</p>
     <input placeholder ="Price" name = "price" />
-    <p class="help is-danger">${getError(errors, 'price')}</p>
-    <input type = "file" name "image" />
+    // <p class="help is-danger">${getError(errors, 'price')}</p>
+    <input type = "file" name = "image" />
     <button>Submit</button>
     <form>
     `
