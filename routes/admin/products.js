0a17888716b494c920cb26d1d877aca9de502e02
Jasper Wong <wlcjasper@gmail.com>
2023-12-13 15:50:16 +0800

    add error check

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -7,18 +7,26 @@ const productsRepo = require('../../repositories/products')
 const productsNewTemplate = require('../../views/products/new')
 
 const {
-
+  requireTitle,
+  requirePrice
 } = require('./validators')
 
 router.get('/admin/products', (req, res) => {
   res.send(productsNewTemplate({}))
 })
 
-router.post('/admin/products', async (req, res) => {
+router.post('/admin/products', [requireTitle, requirePrice], async (req, res) => {
+  const errors = validationResult(req)
+  console.log(errors)
+  if (!errors.isEmpty()) {
+    console.log('error!')
+    // return res.send(signUpTemplate({ req, errors }))
+  }
+  if (errors.isEmpty()) {
   const { title, price } = req.body
-  const product = await productsRepo.create({ title, price })
+  await productsRepo.create({ title, price })
   return res.redirect('/admin/products')
-})
+}})
 
 
 router.get('/admin/products/new', (req, res) => {})
