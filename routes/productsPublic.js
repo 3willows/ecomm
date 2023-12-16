c47dc43ba490ebc69c5e3fa50dca68ba431ff6be
Jasper Wong <wlcjasper@gmail.com>
2023-12-16 20:56:52 +0800

    wire up the Shopping Cart

--- routes/productsPublic.js
+++ routes/productsPublic.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const multer = require('multer')
 const crypto = require('crypto')
+const chalk = require('chalk')
 
 const productsRepo = require('../repositories/products')
 const cartRepo = require('../repositories/cart')
 const productsPublicTemplate = require('../views/products/publicList')
+const cartPublicTemplate = require('../views/products/cartList')
 
 const router = express.Router()
 
@@ -13,7 +15,7 @@ const upload = multer({ storage: multer.memoryStorage() })
 router.get('/products/public', async (req, res) => {
   const products = await productsRepo.getAll()
   if (!req.session.id) {
-  req.session.id = crypto.randomBytes(5).toString('hex')
+    req.session.id = crypto.randomBytes(5).toString('hex')
   }
   res.send(productsPublicTemplate({ products }))
 })
@@ -26,6 +28,30 @@ router.post('/cart/products/:id', async (req, res) => {
   const productId = req.params.id
   cartRepo.create(sessionId, productId)
   res.redirect('/products/public')
+})
+
+router.get('/cart', async (req, res) => {
+  // console.log('ello')
+  const cartForUser = await cartRepo.getOne(req.session.id)
+  if (!cartForUser) {
+    console.log(chalk.red('no session yet'))
+    res.redirect('/products/public')
+  }
+  const cartView = []
+  if (cartForUser ) {
+    if (cartForUser.products)
+    for (let entry of cartForUser.products) {
+      const product = await productsRepo.getOne(entry[0])
+      const title = product.title
+      const image = product.image
+      const price = product.price
+      const quantity = entry[1]
+      const cartViewEntry = { title, price, quantity, image }
+      cartView.push(cartViewEntry)
+    }
+  res.send(cartPublicTemplate(cartView))
+  }
+
 })
 
 module.exports = router
